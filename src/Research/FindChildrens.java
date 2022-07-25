f7664a8e53c4b0499b536aa5097dab18fcec778e
Stepan <St3Ba4@gmail.com>
2022-07-25 11:14:25 +0300

    Refactoring researchs

--- src/Research/FindChildrens.java
+++ src/Research/FindChildrens.java
@@ -6,24 +6,15 @@ import FamilyTree.src.Core.Models.Person.Person;
 
 import java.util.ArrayList;
 
-public class FindChildrens implements Research {
-    Storage object;
-    Person subject;
-
+public class FindChildrens extends FindRelatoves {
     public FindChildrens(Storage object, Person subject) {
-        this.object = object;
-        this.subject = subject;
+        super(object, subject);
     }
 
     @Override
     public ArrayList<Person> startResearch() {
         ArrayList<Person> childrens = new ArrayList<>();
-        for (Person relative :
-                subject.getRelatives(Kinship.DESCENDANT)) {
-            if (relative.getKinship(subject).getGeneration() == 0) {
-                childrens.add(relative);
-            }
-        }
+        findRelatives(childrens, Kinship.DESCENDANT, 0);
         return childrens;
     }
 }
---
+++ src/Research/FindRelatoves.java
@@ -0,0 +1,33 @@
+package FamilyTree.src.Research;
+
+import FamilyTree.src.Core.Infrastructure.Storage;
+import FamilyTree.src.Core.Models.Person.Kinship;
+import FamilyTree.src.Core.Models.Person.Person;
+
+import java.util.ArrayList;
+
+public abstract class FindRelatoves implements Research{
+    Storage object;
+    Person subject;
+
+    public FindRelatoves(Storage object, Person subject) {
+        this.object = object;
+        this.subject = subject;
+    }
+
+    @Override
+    public ArrayList<Person> startResearch() {
+        ArrayList<Person> childrens = new ArrayList<>();
+        findRelatives(childrens, Kinship.UNSPECIFIED, -1);
+        return childrens;
+    }
+
+    protected void findRelatives(ArrayList<Person> childrens, Kinship kinship, int generation) {
+        for (Person relative :
+                subject.getRelatives(kinship)) {
+            if (relative.getKinship(subject).getGeneration() == generation) {
+                childrens.add(relative);
+            }
+        }
+    }
+}
