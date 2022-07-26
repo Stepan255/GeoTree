5282e2ae0d0a8650e0a83de7fe44b7e00152a22f
Stepan <St3Ba4@gmail.com>
2022-07-26 18:46:51 +0300

    Add Presenter

---
+++ src/Core/Presenter/Presenter.java
@@ -0,0 +1,40 @@
+package FamilyTree.src.Core.Presenter;
+
+import FamilyTree.src.Core.Infrastructure.Storage;
+import FamilyTree.src.Core.Models.Person.Person;
+import FamilyTree.src.Core.Views.View;
+
+import java.util.ArrayList;
+
+public class Presenter {
+    private View view;
+    private Storage storage;
+
+    public Presenter(View view, Storage storage) {
+        this.view = view;
+        this.storage = storage;
+    }
+
+    public Storage getStorage() {
+        return storage;
+    }
+
+    public  void show(){
+        show(false);
+    }
+
+    public void show(Boolean showId) {
+        for (Person person :
+                this.storage.getPersons()) {
+            String text = "";
+            if(showId){
+                text = person.getId() + ": ";
+            }
+            text += person.toString();
+
+            view.show(String.format("%s\n", text));
+        }
+    }
+
+
+}
